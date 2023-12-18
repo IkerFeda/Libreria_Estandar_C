8fe0c7ceb2ed664585a5d7d14c4cd182d7a46eb5
IkerFeda <iferndac@myuax.com>
2023-12-18 23:42:46 +0100

    .

--- archivos.c
+++ archivos.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    FILE *file = fopen("file.txt", "r");
+    FILE *file = fopen("Hola.txt", "r");
     if (file == NULL) {
         printf("El archivo no se pudo abrir.\n");
         return 1;
     }
 
-    // Cerrar el archivo al final
     fclose(file);
 
     return 0;
